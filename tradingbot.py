a52565d315fbf672eb92f9d427362e1ee0f61964
Jamie-19 <mdl21cs057@mec.ac.in>
2024-06-18 22:11:08 +0530

    .

--- tradingbot.py
+++ tradingbot.py
@@ -1,4 +1,22 @@
+import os
 from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
-from lumibot.strategies._strategy import Strategy
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
+BASE_URL = os.getenv('BASE_URL')
+ALPACA_CREDS={
+    "API_KEY": os.getenv('API_KEY'),
+    "API_SECRET": os.getenv('API_SECRET'),
+    "PAPER": True,
+}
+
+class TradingBot(Strategy):
+    def initialize(self):
+
+
+
+broker=Alpaca(ALPACA_CREDS)
