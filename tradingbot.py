1e027bfd58cfc4bc0d374197c0ef3eb868128b7c
Jamie-19 <mdl21cs057@mec.ac.in>
2024-06-20 18:57:44 +0530

    refactor: Update tradingbot.py with Alpaca credentials and backtest dates

--- tradingbot.py
+++ tradingbot.py
@@ -2,36 +2,45 @@ import os
 from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
-from lumibot.traders import Trader
 from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Environment variables
 BASE_URL = os.getenv("BASE_URL")
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+
 # if not all([BASE_URL, API_KEY, API_SECRET]):
 #     raise ValueError("One or more environment variables are missing!")
 
+# # Print statements for debugging
 # print(f"BASE_URL: {BASE_URL}")
 # print(f"API_KEY: {API_KEY}")
 # print(f"API_SECRET: {API_SECRET}")
 
-ALPACA_CREDS={
-    "API_KEY": os.getenv("API_KEY"),
-    "API_SECRET": os.getenv("API_SECRET"),
+# Alpaca credentials
+ALPACA_CREDS = {
+    "API_KEY": API_KEY,
+    "API_SECRET": API_SECRET,
     "PAPER": True,
 }
 
+symbol = "AAPL"
 # Define the strategy
-
-symbol="SPY"
-
 class MyStrategy(Strategy):
     def initialize(self,symbol):
         self.symbol = symbol
         self.sleep_time = "24H"
         self.last_trade = None
-    def trading(self):
+
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+
+
+    def on_trading_iteration(self):
         if self.last_trade == None:
             order = self.create_order(
                 self.symbol,
@@ -40,22 +49,26 @@ class MyStrategy(Strategy):
                 type="market",
             )
             self.submit_order(order)
-            self.last_trade = "buy"   
-    
-        
-startdate=datetime(2023,12,15)
-enddate=datetime(2023,12,31)
+            self.last_trade = "buy"
 
-broker=Alpaca(ALPACA_CREDS)
+# Backtest dates
+start_date = datetime(2023, 12, 15)
+end_date = datetime(2023, 12, 31)
 
-strategy=MyStrategy(
-    name="MyStrategy",
+# Broker setup
+broker = Alpaca(ALPACA_CREDS)
+
+# Strategy setup
+strategy = MyStrategy(
+    name="MLStrat",
     broker=broker,
-    parameters={"symbol":symbol})
+    parameters={"symbol": symbol}
+)
 
+# Run the backtest
 strategy.backtest(
     YahooDataBacktesting,
-    startdate,
-    enddate,
-    parameters={"symbol":symbol},
-    )
+    start_date,
+    end_date,
+    parameters={"symbol": symbol},
+)
