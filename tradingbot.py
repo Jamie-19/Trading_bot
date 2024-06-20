53d954c248e0408cbc2fb0af8f181654c1511d04
Jamie-19 <mdl21cs057@mec.ac.in>
2024-06-20 19:11:17 +0530

    setting profit and stoploss

--- tradingbot.py
+++ tradingbot.py
@@ -28,28 +28,36 @@ ALPACA_CREDS = {
 }
 
 symbol = "AAPL"
+cash_at_risk = 0.5
 # Define the strategy
 class MyStrategy(Strategy):
-    def initialize(self,symbol):
+    def initialize(self,symbol,cash_at_risk):
         self.symbol = symbol
         self.sleep_time = "24H"
         self.last_trade = None
+        self.cash_at_risk = cash_at_risk
 
     def position_sizing(self):
         cash = self.get_cash()
         last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price,0)
+        return cash, last_price, quantity
 
 
     def on_trading_iteration(self):
-        if self.last_trade == None:
-            order = self.create_order(
-                self.symbol,
-                10,
-                "buy",
-                type="market",
-            )
-            self.submit_order(order)
-            self.last_trade = "buy"
+        cash, last_price, quantity = self.position_sizing()
+        if cash>last_price:
+            if self.last_trade == None:
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type="bracket",
+                    take_profit_price=last_price * 1.20,
+                    stop_loss_price=last_price * 0.95,
+                )
+                self.submit_order(order)
+                self.last_trade = "buy"
 
 # Backtest dates
 start_date = datetime(2023, 12, 15)
@@ -62,7 +70,9 @@ broker = Alpaca(ALPACA_CREDS)
 strategy = MyStrategy(
     name="MLStrat",
     broker=broker,
-    parameters={"symbol": symbol}
+    parameters={"symbol": symbol,
+                "cash_at_risk": cash_at_risk
+                }
 )
 
 # Run the backtest
@@ -70,5 +80,6 @@ strategy.backtest(
     YahooDataBacktesting,
     start_date,
     end_date,
-    parameters={"symbol": symbol},
+    parameters={"symbol": symbol,
+                "cash_at_risk": cash_at_risk},
 )
